737ad450b6555cbc5caf5647bce4122a9d120e06
Maksim Buldukyan <mbuldukyan@sfxdx.ru>
2019-03-21 18:27:32 +0700

    home dir fixes for root

--- rund.sh
+++ rund.sh
@@ -1,5 +1,5 @@
 IMAGE="${IMAGE:-sevenswen/crypticcoinubuntu18.04}"
 docker run -d -e USER_ID=$(id -u $USER) -e GROUP_ID=$(id -g $USER) \
-    -v /home/$USER/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
-    -v /home/$USER/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
+    -v $HOME/$USER/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
+    -v $HOME/$USER/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
     --rm -it --name cryptic $IMAGE "$@"
