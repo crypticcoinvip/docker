2c040e8b9c1bfdbadcfaae85e11b9c474d42b822
Maksim Buldukyan <mbuldukyan@sfxdx.ru>
2019-03-21 19:16:36 +0700

    mn_announce.sh fix

--- mn_announce.sh
+++ mn_announce.sh
@@ -41,16 +41,16 @@ ERRFILE=/tmp/err.$$
 function run_docker {
     IMAGE="${IMAGE:-sevenswen/crypticcoinubuntu18.04}"
     docker run -d -e USER_ID=$(id -u $USER) -e GROUP_ID=$(id -g $USER) \
-        -v $HOME/$USER/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
-        -v $HOME/$USER/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
+        -v $HOME/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
+        -v $HOME/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
         --rm -it --name cryptic $IMAGE "$@"
 }
 
 function run_docker_i {
     IMAGE="${IMAGE:-sevenswen/crypticcoinubuntu18.04}"
     docker run -e USER_ID=$(id -u $USER) -e GROUP_ID=$(id -g $USER) \
-        -v $HOME/$USER/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
-        -v $HOME/$USER/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
+        -v $HOME/.crypticcoin/:/home/crypticuser/.crypticcoin:rw \
+        -v $HOME/.crypticcoin-params/:/home/crypticuser/.crypticcoin-params:rw \
         --rm -it --name cryptic $IMAGE "$@"
 }
 
